e3df0820cc4243e66006092710818fc97cdf5815
Mortix4 <footballwithian@gmail.com>
2024-03-05 21:13:16 +0200

    Create wpm.py
    initial commit with base code

---
+++ wpm.py
@@ -0,0 +1,22 @@
+import time
+import pygame
+
+class game:
+    def __init__(self, _time, _):
+        self.time = _time
+    
+    def resetGame(self):
+        pass
+    
+    def startGame(self):
+        pass
+        
+
+def main():
+    pass
+    
+    
+
+
+if __name__ == "__main__":
+    main()
